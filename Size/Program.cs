480a24ab9dfc055b3076c85b51ec2ee8b77e3624
Itaru <litorud@live.jp>
2018-11-10 21:10:11 +0900

    クラスを分ける意味がいったんなくなったので Sizer を削除しました。

--- Size/Program.cs
+++ Size/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -13,10 +14,13 @@ namespace Size
 {
     public partial class Program : Application
     {
-        internal static string PowerShellArguments { get; } = "-noexit -Command Set-Location " + AppDomain.CurrentDomain.BaseDirectory;
-
         [DllImport("user32.dll")]
         private static extern int GetWindowRect(IntPtr hWnd, out RECT lpRECT);
+
+        [DllImport("user32.dll")]
+        private static extern int MoveWindow(IntPtr hWnd, int x, int y, int nWidth, int nHeight, int bRepaint);
+
+        public static string PowerShellArguments { get; } = $"-noexit -Command Set-Location '{AppDomain.CurrentDomain.BaseDirectory}'";
 
         protected override void OnStartup(StartupEventArgs e)
         {
@@ -26,7 +30,7 @@ namespace Size
                     ShowSizes();
                     break;
                 case 5:
-                    new Sizer().SetSize(e.Args);
+                    SetSize(e.Args);
                     break;
                 default:
                     ShowHelp();
@@ -79,6 +83,41 @@ namespace Size
         private static void WriteBounds(string title, double x, double y, double width, double height)
         {
             Console.WriteLine($"{title}: ({x}, {y}) {width}×{height}");
+        }
+
+        public void SetSize(IList<string> args)
+        {
+            var title = args[0];
+            var regex = new Regex(title, RegexOptions.Compiled);
+
+            var targetProcess = Process.GetProcesses()
+                .Where(p => p.MainWindowHandle.ToInt64() > 0 && regex.IsMatch(p.MainWindowTitle))
+                .FirstOrDefault();
+
+            if (targetProcess == null)
+            {
+                Console.WriteLine("対象ウィンドウが見つかりません。");
+                return;
+            }
+
+            int x = int.Parse(args[1]);
+            int y = int.Parse(args[2]);
+            int width = int.Parse(args[3]);
+            int height = int.Parse(args[4]);
+            MoveWindow(targetProcess.MainWindowHandle, x, y, width, height, 1);
+
+            UpdateJumpList(args);
+        }
+
+        private void UpdateJumpList(IEnumerable<string> args)
+        {
+            var arguments = ArgumentEscaper.EscapeAndConcatenate(args);
+
+            JumpList.AddToRecentCategory(new JumpTask
+            {
+                Title = arguments,
+                Arguments = arguments
+            });
         }
 
         private static void ShowHelp()
