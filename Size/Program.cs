3e8e4e29dc048cde8839069a009ff5d3b6fb97ff
Itaru <litorud@live.jp>
2018-11-11 04:26:02 +0900

    コマンドライン引数のパースの仕方を変更しました。

--- Size/Program.cs
+++ Size/Program.cs
@@ -24,22 +24,142 @@ namespace Size
 
         protected override void OnStartup(StartupEventArgs e)
         {
-            switch (e.Args.Length)
+            var commandLineApplication = new CommandLineApplication(false)
             {
-                case 0:
-                    ShowSizes();
-                    break;
-                case 5:
-                    SetSize(e.Args);
-                    break;
-                default:
-                    ShowHelp();
-                    break;
-            }
+                OptionsComparison = StringComparison.CurrentCultureIgnoreCase
+            };
 
+            var titleArgument = commandLineApplication.Argument("title", "この文字列を含むタイトルを持つウィンドウが変更対象です。");
+
+            var isRegexOption = commandLineApplication.Option("-r|--regex", "title を正規表現として解釈します。", CommandOptionType.NoValue);
+
+            var showsHelpOption = commandLineApplication.Option("-h|--help|-?", "ヘルプを表示します。", CommandOptionType.NoValue);
+
+            var showsListOption = commandLineApplication.Option("-l|--list", "ウィンドウの一覧を出力します。", CommandOptionType.NoValue);
+
+            commandLineApplication.OnExecute(() =>
+            {
+                var 何もしてない = true;
+
+                if (!string.IsNullOrEmpty(titleArgument.Value))
+                {
+                    SetSize(titleArgument.Value, commandLineApplication.RemainingArguments, isRegexOption.HasValue());
+                    UpdateJumpList(e.Args);
+                    何もしてない = false;
+                }
+
+                if (showsHelpOption.HasValue())
+                {
+                    ShowHelp();
+                    何もしてない = false;
+                }
+
+                if (showsListOption.HasValue())
+                {
+                    ShowSizes();
+                    何もしてない = false;
+                }
+
+                if (何もしてない)
+                {
+                    ShowHelp();
+                    Console.WriteLine();
+                    ShowSizes();
+                }
+            });
+
+            commandLineApplication.Execute(e.Args);
 #if !DEBUG
             Shutdown();
 #endif
+        }
+
+        public void SetSize(string title, IList<string> remainingArguments, bool isRegex)
+        {
+            var targetProcess = GetTargetProcess(title, isRegex);
+
+            if (targetProcess == null)
+            {
+                Console.WriteLine("対象ウィンドウが見つかりません。");
+                return;
+            }
+
+            (int x, int y, int width, int height) = GetArguments(remainingArguments, targetProcess.MainWindowHandle);
+
+            MoveWindow(targetProcess.MainWindowHandle, x, y, width, height, 1);
+        }
+
+        private Process GetTargetProcess(string title, bool isRegex)
+        {
+            var processes = Process.GetProcesses().Where(p => p.MainWindowHandle.ToInt64() > 0);
+
+            if (isRegex)
+            {
+                var regex = new Regex(title, RegexOptions.Compiled);
+                return processes.FirstOrDefault(p => regex.IsMatch(p.MainWindowTitle));
+            }
+            else
+            {
+                return processes.FirstOrDefault(p => p.MainWindowTitle.IndexOf(title, StringComparison.CurrentCultureIgnoreCase) >= 0);
+            }
+        }
+
+        private (int x, int y, int width, int height) GetArguments(IList<string> args, IntPtr handle)
+        {
+            switch (args.Count)
+            {
+                case 0:
+                    GetWindowRect(handle, out var rect0);
+                    return (rect0.Left, rect0.Top, rect0.Right - rect0.Left, rect0.Bottom - rect0.Top);
+                case 1:
+                    GetWindowRect(handle, out var rect1);
+                    return (int.Parse(args[0]), rect1.Top, rect1.Right - rect1.Left, rect1.Bottom - rect1.Top);
+                case 2:
+                    GetWindowRect(handle, out var rect2);
+                    return (int.Parse(args[0]), int.Parse(args[1]), rect2.Right - rect2.Left, rect2.Bottom - rect2.Top);
+                case 3:
+                    GetWindowRect(handle, out var rect3);
+                    return (int.Parse(args[0]), int.Parse(args[1]), int.Parse(args[2]), rect3.Bottom - rect3.Top);
+                default:
+                    return (int.Parse(args[0]), int.Parse(args[1]), int.Parse(args[2]), int.Parse(args[3]));
+            }
+        }
+
+        private void UpdateJumpList(IEnumerable<string> args)
+        {
+            var arguments = ArgumentEscaper.EscapeAndConcatenate(args);
+
+            JumpList.AddToRecentCategory(new JumpTask
+            {
+                Title = arguments,
+                Arguments = arguments
+            });
+        }
+
+        private static void ShowHelp()
+        {
+            Console.WriteLine(@"Size
+
+指定したタイトルを持つウィンドウを、指定した位置とサイズに変更します。
+
+構文
+    Size.exe [-r] [-l] [-h] <title> <x> <y> <width> <height>
+
+    title : この文字列を含むタイトルを持つウィンドウが変更対象です。
+    x     : 変更後のウィンドウ左上の X 座標。
+    y     : 変更後のウィンドウ左上の Y 座標。
+    width : 変更後のウィンドウの幅。
+    height: 変更後のウィンドウの高さ。
+
+    以下のオプションがあります。
+
+    -r    : title を正規表現として解釈します。
+    -l    : ウィンドウの一覧を出力します。
+    -h    : このヘルプを表示します。
+
+注意
+    変更するウィンドウは最初に該当した1つだけです。
+    オプションは x より前に指定する必要があります。");
         }
 
         private static void ShowSizes()
@@ -83,60 +203,6 @@ namespace Size
         private static void WriteBounds(string title, double x, double y, double width, double height)
         {
             Console.WriteLine($"{title}: ({x}, {y}) {width}×{height}");
-        }
-
-        public void SetSize(IList<string> args)
-        {
-            var title = args[0];
-            var regex = new Regex(title, RegexOptions.Compiled);
-
-            var targetProcess = Process.GetProcesses()
-                .Where(p => p.MainWindowHandle.ToInt64() > 0 && regex.IsMatch(p.MainWindowTitle))
-                .FirstOrDefault();
-
-            if (targetProcess == null)
-            {
-                Console.WriteLine("対象ウィンドウが見つかりません。");
-                return;
-            }
-
-            int x = int.Parse(args[1]);
-            int y = int.Parse(args[2]);
-            int width = int.Parse(args[3]);
-            int height = int.Parse(args[4]);
-            MoveWindow(targetProcess.MainWindowHandle, x, y, width, height, 1);
-
-            UpdateJumpList(args);
-        }
-
-        private void UpdateJumpList(IEnumerable<string> args)
-        {
-            var arguments = ArgumentEscaper.EscapeAndConcatenate(args);
-
-            JumpList.AddToRecentCategory(new JumpTask
-            {
-                Title = arguments,
-                Arguments = arguments
-            });
-        }
-
-        private static void ShowHelp()
-        {
-            Console.WriteLine(@"Size
-
-指定したタイトルを持つウィンドウを、指定した位置とサイズに変更します。
-
-構文
-    Size.exe <title> <x> <y> <width> <height>
-
-    title : 正規表現で指定します。これに該当するタイトルを持つウィンドウが変更対象です。
-    x     : 変更後のウィンドウ左上の X 座標。
-    y     : 変更後のウィンドウ左上の Y 座標。
-    width : 変更後のウィンドウの幅。
-    height: 変更後のウィンドウの高さ。
-
-注意
-    変更するウィンドウは最初に該当した1つだけです。");
         }
 
         [StructLayout(LayoutKind.Sequential)]
